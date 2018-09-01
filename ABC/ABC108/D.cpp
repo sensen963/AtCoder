7cca50ffa509252c333fbccca90b9055a229acf2
sen <sen@MacBook-Pro.local>
2018-09-02 02:35:59 +0900

    ABC108

--- ABC/ABC108/D.cpp
+++ ABC/ABC108/D.cpp
@@ -26,5 +26,41 @@ typedef long long unsigned int ll;
 
 int main() {
 
+  ll L;
+  cin >> L;
+
+  int r = 0;
+  while (pow(2, r) <= L) r++;
+  r--;
+
+  vector<int> vr, vl, w;
+  for (int i = r; i >= 0; i--) {
+    if (L - pow(2, r) - pow(2, i - 1) >= 0) {
+      vr.push_back(i);
+      vl.push_back(r + 1);
+      w.push_back(L - pow(2, i - 1));
+      L -= pow(2, i - 1);
+    }
+  }
+
+  // for (int i = 0; i != vr.size(); i++) {
+  //   cout << vr[i] << vl[i] << w[i] << endl;
+  // }
+  for (int i = 1; i <= r; i++){
+    vr.push_back(i);
+    vr.push_back(i);
+    vl.push_back(i + 1);
+    vl.push_back(i + 1);
+    w.push_back(pow(2, i - 1));
+    w.push_back(0);
+  }
+
+  cout << (r + 1) << " " << vr.size() << endl;
+  for (int i = 0; i != vr.size(); i++){
+    cout << vr[i] << " " << vl[i] << " " << w[i] << endl;
+  }
+
+
+
   return 0;
 }
