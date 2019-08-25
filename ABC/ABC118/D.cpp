b250b9ca752a1d3c43f1c555f865015c9e0d714a
sen <nanablue2035@gmail.com>
2019-08-25 22:50:26 +0900

    ABC118

--- ABC/ABC118/D.cpp
+++ ABC/ABC118/D.cpp
@@ -16,6 +16,7 @@
 #include <numeric>
 using namespace std;
 typedef long long int ll;
+typedef pair<int, int> P;
 
 #define EPS (1e-7)
 #define INF 1e18
@@ -27,6 +28,48 @@ typedef long long int ll;
 
 
 int main() {
+    vector<int> a = {2,5,5,4,5,6,3,7,6};
+    ll N, M;
+    cin >> N >> M;
+    vector<P> A(M);
+    int argmin = 0;
+    REP(i, M) {
+        cin >> A[i].first;
+        A[i].second = a[A[i].first - 1];
+        if ((A[i].second == A[argmin].second && A[i].first > A[argmin].first) || A[i].second < A[argmin].second) argmin = i;
+    }
+    int std = A[argmin].first;
+    int minc = A[argmin].second;
+
+    sort(A.begin(), A.end(), [](P r, P l){
+        return (r.first > l.first);
+    });
+
+    vector<ll> dp(N + 1, -1);
+    dp[0] = 0;
+    REP(i, N + 1){
+        if (i == 0) continue;
+        ll tmp1 = -1;
+        for(auto tmp : A){
+            if (i - tmp.second >= 0) if (dp[i - tmp.second] != -1) tmp1 = max(tmp1, dp[i - tmp.second] + 1);
+        }
+        dp[i] = tmp1;
+    }
+    //REP(i, N + 1)cout << dp[i] << endl;
+    vector<int> s;
+    ll red = N;
+    REP(i, dp[N]){
+        for(auto tmp : A){
+            if (red - tmp.second >= 0) if (dp[red - tmp.second] == dp[red] - 1){
+                s.push_back(tmp.first);
+                red -= tmp.second;
+                break;
+            }
+        }
+    }
+
+    REP(i, dp[N])cout << s[i];
+    cout << endl;
 
     return 0;
 }
