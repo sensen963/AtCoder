c0cd9e750408aa100836466c8bb1a1e33ae57c67
sen <nanablue2035@gmail.com>
2020-11-29 13:59:51 +0900

    ABC175

--- ABC/ABC175/D.cpp
+++ ABC/ABC175/D.cpp
@@ -24,10 +24,56 @@ typedef long long int ll;
 #define min(p,q)((p)<(q)?(p):(q))
 #define PI (acos(-1))
 
-#define REP(i, n) for(int i = 0; i < (int)(n); i++)
+#define REP(i, n) for(int i = 0; i < (ll)(n); i++)
 #define rep(i, init, n) for(int i = init; i <(int)(n); i++)
 
 int main() {
+
+    ll N, K;
+    cin >> N >> K;
+    vector<ll> P(N), C(N);
+    REP(i, N) {
+        cin >> P[i];
+        P[i]--;
+    }
+    REP(i, N) cin >> C[i];
+    
+    ll ans = -INF;
+    REP(init, N){
+        vector<ll> score(N, -INF);
+        vector<ll> visited(N, -1);
+
+        visited[init] = 0;
+        ll from = init;
+        ll to;
+        REP(i, K){
+            to = P[from];
+            if (visited[to] >= 0) {
+                ll interval = i + 1 - visited[to];
+                ll diff = (to != init) ? score[from] + C[to] - score[to]:score[from] + C[to] ;
+                if (diff <= 0) break;
+                ll vonus = (((K - visited[to]) / interval) -1) * diff;
+                ll res = (K - visited[to]) % interval;
+                ll tmp = -INF;
+                ll from1 = to;
+                ll to1;
+                REP(j, interval){
+                    to1 = P[from1];
+                    score[to1] += vonus;
+                    from1 = to1;
+                }
+                i += (((K - visited[to]) / interval) -1) * interval;
+                vector<ll> visited1(N, -1);
+                visited = visited1;
+            }
+            if (i != 0) score[to] = score[from] + C[to];
+            else score[to] = C[to];
+            visited[to] = i + 1;
+            from = to;
+        }
+        REP(i, N) ans = max(ans, score[i]);
+    }
+    cout << ans << endl;
 
     return 0;
 }
--- ABC/ABC175/E.cpp
+++ ABC/ABC175/E.cpp
@@ -28,6 +28,32 @@ typedef long long int ll;
 #define rep(i, init, n) for(int i = init; i <(int)(n); i++)
 
 int main() {
+    ll R, C, K;
+    cin >> R >> C >> K;
+    vector<vector<ll>> P(R, vector<ll>(C, 0));
+    vector<ll> v(K);
+    ll a, b;
+    REP(i, K) {
+        cin >> a >> b >> v[i];
+        a--;b--;
+        P[a][b] = v[i];
+    }
 
+    vector<vector<vector<ll>>> dp(R+2, vector<vector<ll>>(C+2, vector<ll>(4, 0)));
+    REP(i, R) REP(j, C) REP(k, 4) {
+        if (P[i][j] > 0 && k < 3) {
+            dp[i][j+1][k+1] = max(dp[i][j+1][k+1], dp[i][j][k] + P[i][j]);
+            dp[i+1][j][0] = max(dp[i+1][j][0], dp[i][j][k] + P[i][j]);
+        }
+        dp[i][j+1][k] = max(dp[i][j+1][k], dp[i][j][k]);
+        dp[i+1][j][0] = max(dp[i+1][j][0], dp[i][j][k]);
+        
+    }
+
+    //ll ans = max(dp[R-1][C][0], max(dp[R-1][C][1], max(dp[R-1][C][2], dp[R-1][C][3])));
+    ll ans = dp[R][C-1][0];
+    cout << ans << endl;
+
+    
     return 0;
 }
